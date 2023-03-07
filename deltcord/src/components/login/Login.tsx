8c9e167b31017f88466774fdd809444f390f5ab9
Vishnu Kulkarni <vishnu@starapps.studio>
2023-03-07 22:44:29 +0530

    basic style and layout for register mix component

--- deltcord/src/components/login/Login.tsx
+++ deltcord/src/components/login/Login.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [newuser, setNewUser] = useState(false)
     return (
         <div className=' flex flex-col justify-center items-center'>
 
-            <h3>Login</h3>
+            <h3>{newuser ? "Register" : "Login"}</h3>
             <div className="flex flex-col w-full border-opacity-50">
                 <div className="grid h-100 card  rounded-box place-items-center">
 
@@ -13,13 +14,31 @@ const Login = () => {
                             <label htmlFor="username"> Username</label>
                             <input type="text" placeholder="Type username  here" className="input w-full max-w-xs" />
                         </div>
+
+                        {newuser ?
+
+                            <div className="flex flex-col">
+                                <label htmlFor="email">Email</label>
+                                <input type="text" placeholder="Type Password  here" className="input w-full max-w-xs" />
+                            </div> : ""}
                         <div className="flex flex-col">
                             <label htmlFor="password">Password</label>
                             <input type="text" placeholder="Type Password  here" className="input w-full max-w-xs" />
                         </div>
-                        <div className="flex gap-1 justify-center items-center ">
-                            <button className="btn btn-success">submit</button>
+                        {newuser ? <div className="flex flex-col">
+                            <label htmlFor="confirm_password"> Confirm Password</label>
+                            <input type="text" placeholder="confirm Password  here" className="input w-full max-w-xs" />
                         </div>
+                            : ''
+                        }
+
+                        <div className="flex gap-1 justify-center items-center ">
+                            <button className="btn btn-success btn-sm">submit</button>
+                        </div>
+                        <button className='btn btn-ghost' onClick={(e) => {
+                            e.preventDefault()
+                            setNewUser(!newuser)
+                        }}>{newuser ? "Existing user" : "register"}</button>
                     </form>
                 </div>
                 <div className="divider">OR</div>
