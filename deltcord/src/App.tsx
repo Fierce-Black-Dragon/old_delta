a7760353451ed0f454fbc3caeef8746a7ed5b086
Vishnu Kulkarni <vishnu@starapps.studio>
2023-03-07 19:19:53 +0530

    initial react ts  and tailwind css setup

---
+++ deltcord/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <div className="bg-black min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        delta cord front end
+      </h1>
+    </div>
+  )
+}
+
+export default App
