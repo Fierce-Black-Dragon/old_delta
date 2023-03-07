876db3904e691307580db080e0eeee8e287a0979
Vishnu Kulkarni <vishnu@starapps.studio>
2023-03-07 19:25:21 +0530

    daisy ui added

--- deltcord/src/App.tsx
+++ deltcord/src/App.tsx
@@ -6,7 +6,9 @@ function App() {
     <div className="bg-black min-h-screen flex justify-center items-center">
       <h1 className="text-3xl font-bold text-blue-600">
         delta cord front end
+
       </h1>
+      <button className="btn btn-accent btn-outline">Three</button>
     </div>
   )
 }
